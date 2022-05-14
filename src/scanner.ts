36d377d8c599098b92ead94195be6dbc0f4c012a
ishiahirake <ishiahirake@gmail.com>
2022-05-14 18:12:30 +0900

    Implments Scanner.

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,10 +1,304 @@
-type Token = {}
+import { error } from "./utils"
+
+enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // One or two character tokens.
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords.
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  EOF,
+}
+
+const keywords: { [key: string]: TokenType } = {
+  and: TokenType.AND,
+  class: TokenType.CLASS,
+  else: TokenType.ELSE,
+  false: TokenType.FALSE,
+  for: TokenType.FOR,
+  fun: TokenType.FUN,
+  if: TokenType.IF,
+  nil: TokenType.NIL,
+  or: TokenType.OR,
+  print: TokenType.PRINT,
+  return: TokenType.RETURN,
+  super: TokenType.SUPER,
+  this: TokenType.THIS,
+  true: TokenType.TRUE,
+  var: TokenType.VAR,
+  while: TokenType.WHILE,
+} as const
+
+class Token {
+  constructor(
+    public readonly type: TokenType,
+    public readonly lexeme: string,
+    public readonly literal: any,
+    public readonly line: number
+  ) {}
+
+  toString(): string {
+    return `${this.type} ${this.lexeme} ${this.literal}`
+  }
+}
 
 export class Scanner {
+  private tokens: Token[] = []
+
+  private start = 0
+  private current = 0
+  private line = 1
+
   constructor(public readonly source: string) {}
 
   scanTokens(): Token[] {
-    // TBD
-    return []
+    while (!this.isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      this.start = this.current
+      this.scanToken()
+    }
+
+    this.addRawToken(TokenType.EOF, "")
+
+    return this.tokens
+  }
+
+  isAtEnd(): boolean {
+    return this.current >= this.source.length
+  }
+
+  scanToken() {
+    const c = this.advance()
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN)
+        break
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN)
+        break
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE)
+        break
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE)
+        break
+      case ",":
+        this.addToken(TokenType.COMMA)
+        break
+      case ".":
+        this.addToken(TokenType.DOT)
+        break
+      case "-":
+        this.addToken(TokenType.MINUS)
+        break
+      case "+":
+        this.addToken(TokenType.PLUS)
+        break
+      case ";":
+        this.addToken(TokenType.SEMICOLON)
+        break
+      case "*":
+        this.addToken(TokenType.STAR)
+        break
+      case "!":
+        this.addToken(this.match("=") ? TokenType.BANG_EQUAL : TokenType.BANG)
+        break
+      case "=":
+        this.addToken(this.match("=") ? TokenType.EQUAL_EQUAL : TokenType.EQUAL)
+        break
+      case "<":
+        this.addToken(this.match("=") ? TokenType.LESS_EQUAL : TokenType.LESS)
+        break
+      case ">":
+        this.addToken(
+          this.match("=") ? TokenType.GREATER_EQUAL : TokenType.GREATER
+        )
+        break
+      case "/":
+        if (this.match("/")) {
+          // A comment goes until the end of the line.
+          while (this.peek() != "\n" && !this.isAtEnd()) {
+            this.advance()
+          }
+        } else {
+          this.addToken(TokenType.SLASH)
+        }
+        break
+
+      case " ":
+      case "\r":
+      case "\t":
+        // Ignore whitespace.
+        break
+
+      case "\n":
+        this.line++
+        break
+
+      case '"':
+        this.string()
+        break
+      default:
+        if (this.isDigit(c)) {
+          this.number()
+        } else if (this.isAlpha(c)) {
+          this.identifier()
+        } else {
+          error(this.line, "Unexpected character.")
+        }
+        break
+    }
+  }
+
+  advance() {
+    return this.source[this.current++]
+  }
+
+  match(expected: string): boolean {
+    if (this.isAtEnd()) {
+      return false
+    }
+    if (this.peek() != expected) {
+      return false
+    }
+
+    this.current++
+
+    return true
+  }
+
+  peek(): string {
+    return this.__peek(0)
+  }
+
+  peekNext(): string {
+    return this.__peek(1)
+  }
+
+  __peek(offset: number): string {
+    if (this.current + offset >= this.source.length) {
+      return "\0"
+    }
+
+    return this.source[this.current + offset]
+  }
+
+  isDigit(c: string): boolean {
+    return "0" <= c && c <= "9"
+  }
+
+  isAlpha(c: string): boolean {
+    return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || c == "_"
+  }
+
+  isAlphaNumeric(c: string): boolean {
+    return this.isAlpha(c) || this.isDigit(c)
+  }
+
+  string() {
+    while (this.peek() != '"' && !this.isAtEnd()) {
+      if (this.peek() == "\n") {
+        this.line++
+      }
+      this.advance()
+    }
+
+    if (this.isAtEnd()) {
+      error(this.line, "Unterminated string.")
+      return
+    }
+
+    // The closing ".
+    this.advance()
+
+    // Trim the surrounding quotes.
+    const value = this.source.substring(this.start + 1, this.current - 1)
+    this.addToken(TokenType.STRING, value)
+  }
+
+  number() {
+    while (this.isDigit(this.peek())) {
+      this.advance()
+    }
+
+    // Look for a fractional part.
+    if (this.peek() == "." && this.isDigit(this.peekNext())) {
+      // Consume the "."
+      this.advance()
+
+      while (this.isDigit(this.peek())) {
+        this.advance()
+      }
+    }
+
+    this.addToken(
+      TokenType.NUMBER,
+      parseFloat(this.source.substring(this.start, this.current))
+    )
+  }
+
+  identifier() {
+    while (this.isAlphaNumeric(this.peek())) {
+      this.advance()
+    }
+
+    const text = this.source.substring(this.start, this.current)
+    const type = keywords[text] ?? TokenType.IDENTIFIER
+
+    this.addToken(type)
+  }
+
+  addToken(type: TokenType, literal: any = null) {
+    const text = this.source.substring(this.start, this.current)
+    this.addRawToken(type, text, literal)
+  }
+
+  addRawToken(
+    type: TokenType,
+    lexeme: string,
+    literal: any = null,
+    line = this.line
+  ) {
+    this.tokens.push(new Token(type, lexeme, literal, line))
   }
 }
